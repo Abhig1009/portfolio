9ebdad47f6faff31c86b39335dd0b18ae3fbe331
Abhig1009 <98938147+Abhig1009@users.noreply.github.com>
2023-04-20 12:31:32 +0530

    Portfolio Website

---
+++ main.js
@@ -0,0 +1,11 @@
+// Header Scroll
+let nav=document.querySelector(".navbar");
+window.onscroll = function() {
+	if(document.documentElement.scrollTop>20){
+		nav.classList.add("header-scrolled");
+	}else{
+		nav.classList.remove("header-scrolled");
+	}
+}
+
+// Moise Hover design
